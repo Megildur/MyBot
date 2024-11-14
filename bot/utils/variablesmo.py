2ca9ed05b440d8bfb0da0f2331945be437f7ec7f
Megildur <merculiefden95@gmail.com>
2024-11-14 22:31:12 +0000

    Variables for media only embed

---
+++ bot/utils/variablesmo.py
@@ -0,0 +1,28 @@
+import discord
+
+color_map = {
+    "discord.Color.blue()": discord.Color.blue(),
+    "discord.Color.blurple()": discord.Color.blurple(),
+    "discord.Color.dark_blue()": discord.Color.dark_blue(),
+    "discord.Color.dark_gold()": discord.Color.dark_gold(),
+    "discord.Color.dark_gray()": discord.Color.dark_gray(),
+    "discord.Color.dark_green()": discord.Color.dark_green(),
+    "discord.Color.dark_magenta()": discord.Color.dark_magenta(),
+    "discord.Color.dark_orange()": discord.Color.dark_orange(),
+    "discord.Color.dark_purple()": discord.Color.dark_purple(),
+    "discord.Color.dark_red()": discord.Color.dark_red(),
+    "discord.Color.dark_teal()": discord.Color.dark_teal(),
+    "discord.Color.darker_gray()": discord.Color.darker_gray(),
+    "discord.Color.gold()": discord.Color.gold(),
+    "discord.Color.green()": discord.Color.green(),
+    "discord.Color.greyple()": discord.Color.greyple(),
+    "discord.Color.light_gray()": discord.Color.light_gray(),
+    "discord.Color.lighter_gray()": discord.Color.lighter_gray(),
+    "discord.Color.magenta()": discord.Color.magenta(),
+    "discord.Color.orange()": discord.Color.orange(),
+    "discord.Color.purple()": discord.Color.purple(),
+    "discord.Color.pink()": discord.Color.pink(),
+    "discord.Color.red()": discord.Color.red(),
+    "discord.Color.teal()": discord.Color.teal(),
+    "discord.Color.yellow()": discord.Color.yellow()
+}
